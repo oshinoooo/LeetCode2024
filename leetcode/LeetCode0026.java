0c9b8a6b079131a2cc9d1920b19c9ee8adbe0c5b
Oshino <1036858975@qq.com>
2024-03-10 13:43:15 +0800

    fix

---
+++ leetcode/LeetCode0026.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode0026 {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int newLength = removeDuplicates(nums);
+        System.out.println("--------------------");
+        System.out.println("newLength: " + newLength + ", nums: " + Arrays.toString(nums));
+        System.out.println("-------------------- ");
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int size = nums.length;
+        int slow = 0;
+        for (int fast = 1; fast < size; ++fast) {
+            if (nums[slow] < nums[fast]) {
+                nums[slow + 1] = nums[fast];
+                slow = slow + 1;
+            }
+        }
+        return slow + 1;
+    }
+
+}
