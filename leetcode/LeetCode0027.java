cc42fce35156fc5138b9e451c6e83b09e1bdd447
Oshino <1036858975@qq.com>
2024-03-10 14:02:49 +0800

    fix

--- leetcode/LeetCode0027.java
+++ leetcode/LeetCode0027.java
@@ -15,20 +15,14 @@ public class LeetCode0027 {
 
     public static int removeElement(int[] nums, int val) {
         int size = nums.length;
-        int switchTimes = 0;
-        for (int i = 0; i < size; ++i) {
-            if (nums[i] == val) {
-                for (int m = i + 1; m < size; ++m) {
-                    if (nums[m] != val) {
-                        nums[i] = nums[m];
-                        nums[m] = val;
-                        ++switchTimes;
-                        break;
-                    }
-                }
+        int slow = 0;
+        for (int fast = 0; fast < size; ++fast) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                ++slow;
             }
         }
-        return size - switchTimes;
+        return slow;
     }
 
 }
---
+++ leetcode/LeetCode0080.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class LeetCode0080 {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int length = removeDuplicates(nums);
+        System.out.println("--------------------");
+        System.out.println("length: " + length + ", nums: " + Arrays.toString(nums));
+        System.out.println("--------------------");
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int slow = 0;
+
+
+        return slow;
+    }
+
+}
